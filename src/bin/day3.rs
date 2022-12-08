8883c0846b0c63c309958d9e298dde5f262e9afc
Nicholas Charbonneau <nicholas.charbonneau@gmail.com>
2022-12-08 11:29:37 -0500

    day3

---
+++ src/bin/day3.rs
@@ -0,0 +1,50 @@
+use std::cmp::Ordering;
+use std::fs::File;
+use std::io::{self, BufRead};
+
+fn get_priority(c: char) -> u32 {
+  let code = c as u32;
+  match (code as u32).cmp(&97) {
+    Ordering::Less => code - 38,
+    _ => code - 96,
+  }
+}
+
+fn main() {
+  let file = File::open("data/input-day3.txt").expect("Invalid file");
+  let lines = io::BufReader::new(file).lines();
+
+  let mut totals: [u32; 2] = [0, 0];
+  let mut group = Vec::<String>::new();
+
+  for line in lines {
+    if let Ok(data) = line {
+      // part 1
+      let (c1, c2) = data.split_at((data.len() / 2) as usize);
+      for c1c in c1.chars().into_iter() {
+        if let Some(_) = c2.chars().into_iter().find(|&c_| c_ == c1c) {
+          totals[0] += get_priority(c1c);
+          break;
+        }
+      }
+
+      // part 2
+      group.push(data);
+      if group.len() == 3 {
+        for c1c in group[0].chars().into_iter() {
+          if let Some(_) = group[1].chars().into_iter().find(|&c_| c_ == c1c) {
+            if let Some(_) = group[2].chars().into_iter().find(|&c_| c_ == c1c)
+            {
+              totals[1] += get_priority(c1c);
+              break;
+            }
+          }
+        }
+        group.clear();
+      }
+    }
+  }
+
+  println!("part 1: {}", totals[0]);
+  println!("part 2: {}", totals[1]);
+}
