4d6ce5ddf192f21b8e3744c9f8f6968e286151ed
Nicholas Charbonneau <nicholas.charbonneau@gmail.com>
2022-12-07 20:21:25 -0500

    day 2 solution

---
+++ src/bin/day2.rs
@@ -0,0 +1,46 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+
+fn main() {
+  let file = File::open("data/input-day2.txt").expect("Invalid file");
+  let lines = io::BufReader::new(file).lines();
+
+  let mut rounds = Vec::<[u32; 2]>::new();
+
+  for line in lines {
+    if let Ok(data) = line {
+      let vec: Vec<&str> = data.split(" ").collect();
+
+      match vec[0] {
+        "A" => match vec[1] {
+          "X" => rounds.push([4, 3]),
+          "Y" => rounds.push([8, 4]),
+          "Z" => rounds.push([3, 8]),
+          _ => panic!("Invalid input"),
+        },
+        "B" => match vec[1] {
+          "X" => rounds.push([1, 1]),
+          "Y" => rounds.push([5, 5]),
+          "Z" => rounds.push([9, 9]),
+          _ => panic!("Invalid input"),
+        },
+        "C" => match vec[1] {
+          "X" => rounds.push([7, 2]),
+          "Y" => rounds.push([2, 6]),
+          "Z" => rounds.push([6, 7]),
+          _ => panic!("Invalid input"),
+        },
+        _ => panic!("Invalid input"),
+      };
+    }
+  }
+
+  let totals = rounds
+    .iter()
+    .copied()
+    .reduce(|acc, item| [&acc[0] + &item[0], &acc[1] + &item[1]])
+    .unwrap();
+
+  println!("part 1: {}", totals[0]);
+  println!("part 2: {}", totals[1]);
+}
